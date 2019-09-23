f89f0cd60eac85a5d7b5a6bb0610aea6dfc82bea
w4c4dwsz <w4c4dwsz@outlook.com>
2019-09-23 21:36:42 +0800

    2019年9月23日 实现了SI4463的接收功能

--- si4463_test.c
+++ si4463_test.c
@@ -118,8 +118,17 @@ u8 rx_buf[25];
 /************************** Function Prototypes ******************************/
 int gpio_init(u16 DeviceId);
 void Si4463_Reset(void);
-int Si4463_Init(void);
-u8 spi_byte(unsigned char data);
+void Si4463_Init(void);
+u8 spi_byte(u8 data);
+void spi_write(unsigned char tx_length, unsigned char *p);
+void spi_read(u8 data_length, u8 api_command );
+u8 check_cts(void);
+void fifo_reset(void);
+void spi_write_fifo(void);
+void enable_tx_interrupt(void);
+void clr_interrupt(void);
+void tx_start(void);
+void tx_data(void);
 /************************** Variable Definitions *****************************/
 
 /*
@@ -146,22 +155,24 @@ XGpio Gpio; /* The Instance of the GPIO Driver */
 int main(void)
 {
 	int status;
-	xil_printf("start run the si4463_test");
+	xil_printf("start run the si4463_test\r\n");
 	/* Initialize the GPIO driver  */
 	status = gpio_init(GPIO_ID);
 	if (status != 0)
 	{
 		xil_printf("gpio_init failed, and the return status is %d\r\n",status);
 	}
+	xil_printf("gpio_initilaze success");
 	/*Initialize the Si4463 */
-	//Si4463_Reset();
+	Si4463_Reset();
+	Si4463_Init();
 	while(1)
 	{
-		XGpio_DiscreteSet(&Gpio,GPIO_CHANNEL,SDI);
-		us_delay(10);
-		XGpio_DiscreteClear(&Gpio,GPIO_CHANNEL,SDI);
-		us_delay(50);
+		xil_printf("delay 2s \r\n");
+		s_delay(2);
+		tx_data();
 	}
+
 
 }
 
@@ -192,9 +203,9 @@ int gpio_init(u16 DeviceId)
 		}
 
 	//设置I/O方向
-	XGpio_SetDataDirection(&Gpio, GPIO_CHANNEL,0x03);
+	XGpio_SetDataDirection(&Gpio, GPIO_CHANNEL,0x23);
 	Reg_DIR = XGpio_GetDataDirection(&Gpio, GPIO_CHANNEL);
-	if (Reg_DIR != 0x03)
+	if (Reg_DIR != 0x23)
 	{
 		xil_printf("DIR set Failed\r\n");
 		return -2;
@@ -221,7 +232,7 @@ void Si4463_Reset(void)
 	u8 data = 0;
 	//Power on Reset(POR) Si4463
 	XGpio_DiscreteSet(&Gpio,GPIO_CHANNEL,SDN);
-	us_delay(10);
+	ms_delay(10);
 	XGpio_DiscreteClear(&Gpio,GPIO_CHANNEL,SDN);
 	XGpio_DiscreteSet(&Gpio,GPIO_CHANNEL,nSEL);
 	ms_delay(5);
@@ -229,24 +240,29 @@ void Si4463_Reset(void)
 	for (i = 0; i< 7; i++)
 	{
 		data = spi_byte(RF_POWER_UP_data[i]);
-		xil_printf("the send message is %d, and the message is %d,and the receive message is %d\r\n",
-				i,RF_POWER_UP_data[i],data);
 	}
 	XGpio_DiscreteSet(&Gpio,GPIO_CHANNEL,nSEL);
+	xil_printf("si4463 reset success\r\n");
 }
 
-int Si4463_Init (void)
-{
-	Si4463_Reset();
 
-	return 0;
-}
-
+/*****************************************************************************/
+/**
+*
+* The purpose of this function is to send and receive data through the spi
+*
+* include function
+*
+*	spi_byte
+*	spi_write
+*
+*
+******************************************************************************/
 
 u8 spi_byte(unsigned char data)
 {
 	unsigned char i;
-
+	volatile unsigned char temp;
 	for (i = 0; i < 8; i++)
 	{
 		if (data & 0x80)
@@ -260,14 +276,428 @@ u8 spi_byte(unsigned char data)
 		//SCK = 1;
 		XGpio_DiscreteSet(&Gpio,GPIO_CHANNEL,SCLK);
 
-		if ((XGpio_DiscreteRead(&Gpio,GPIO_CHANNEL) >> 1) & 0x1)
+		temp = XGpio_DiscreteRead(&Gpio,GPIO_CHANNEL) & 0x02;
+		if (temp)
 			data |= 0x01;
 		else
 			data &= 0xfe;
 
 		//SCK = 0;
 		XGpio_DiscreteClear(&Gpio,GPIO_CHANNEL,SCLK);
+
 	}
 	return (data);
 }
+/*---------------------------------------------------------------------------*/
+ /*
+ *
+ *
+ * 向spi上写数据
+ *
+ *
+ * ---------------------------------------------------------------------------*/
+void spi_write(unsigned char tx_length, unsigned char *p)
+{
+	unsigned char i,j;
 
+	i = 0;
+	while(i!=0xff)
+		i = check_cts();
+
+	//SCK = 0;
+	XGpio_DiscreteClear(&Gpio,GPIO_CHANNEL,SCLK);
+	//nSEL = 0;
+	XGpio_DiscreteClear(&Gpio,GPIO_CHANNEL,nSEL);
+	for (i = 0; i < tx_length; i++)
+	{
+		j = *(p+i);
+		spi_byte(j);
+	}
+
+	//nSEL = 1;
+	XGpio_DiscreteSet(&Gpio,GPIO_CHANNEL,nSEL);
+}
+
+/*---------------------------------------------------------------------------*/
+ /*
+ *
+ *
+ * 读SPI上数据
+ *
+ *
+ * ---------------------------------------------------------------------------*/
+void spi_read(u8 data_length, u8 api_command )
+{
+	u8 i;
+
+	u8 p[1];
+	p[0] = api_command;
+	i = 0;
+	while(i!=0xff)
+		i = check_cts();
+
+	spi_write(1, p);
+
+	i = 0;
+	while(i!=0xff)
+		i = check_cts();
+
+	//nSEL = 1;
+	XGpio_DiscreteSet(&Gpio,GPIO_CHANNEL,nSEL);
+	//SCK = 0;
+	XGpio_DiscreteClear(&Gpio,GPIO_CHANNEL,SCLK);
+	//nSEL = 0;
+	XGpio_DiscreteClear(&Gpio,GPIO_CHANNEL,nSEL);
+	spi_byte(0x44);
+	for (i = 0; i< data_length; i++)
+		spi_read_buf[i] = spi_byte(0xff);
+	//nSEL = 1;
+	XGpio_DiscreteSet(&Gpio,GPIO_CHANNEL,nSEL);
+}
+
+
+u8 check_cts(void)
+{
+	u8 i;
+
+	//nSEL = 1;
+	XGpio_DiscreteSet(&Gpio,GPIO_CHANNEL,nSEL);
+	//SCK = 0;
+	XGpio_DiscreteClear(&Gpio,GPIO_CHANNEL,SCLK);
+	//nSEL = 0;
+	XGpio_DiscreteClear(&Gpio,GPIO_CHANNEL,nSEL);
+
+	spi_byte(0x44);
+	i = spi_byte(0);
+	//nSEL = 1;
+	XGpio_DiscreteSet(&Gpio,GPIO_CHANNEL,nSEL);
+	return (i);
+}
+
+void Si4463_Init(void)
+{
+	u8 app_command_buf[20];
+	xil_printf("initialize si4463 \r\n");
+	//spi_write(0x07, RF_GPIO_PIN_CFG_data);
+	app_command_buf[0] = 0x13;			// SET GPIO PORT
+	app_command_buf[1]  = 0x14; 		// gpio 0 ,Rx data
+	app_command_buf[2]  = 0x02;    		// gpio1, output 0
+	app_command_buf[3]  = 0x21;  		// gpio2, hign while in receive mode
+	app_command_buf[4]  = 0x20; 		// gpio3, hign while in transmit mode
+	app_command_buf[5]  = 0x27;   		// nIRQ
+	app_command_buf[6]  = 0x0b;  		// sdo
+	spi_write(7, app_command_buf);
+	xil_printf("SET GPIO PORT SUCCESS\r\n");
+	// spi_write(0x05, RF_GLOBAL_XO_TUNE_1_data);
+    app_command_buf[0] = 0x11;
+	app_command_buf[1]  = 0x00;
+	app_command_buf[2]  = 0x01;
+	app_command_buf[3]  = 0x00;
+	app_command_buf[4]  = 98;  			// freq  adjustment
+	spi_write(5, app_command_buf);
+	xil_printf("Sets the value of a property.\r\n");
+	// spi_write(0x05, RF_GLOBAL_CONFIG_1_data);
+  	app_command_buf[0] = 0x11;
+	app_command_buf[1]  = 0x00;
+	app_command_buf[2]  = 0x01;
+	app_command_buf[3]  = 0x03;
+	app_command_buf[4]  = 0x40;  		// tx = rx = 64 byte,PH,high performance mode
+	spi_write(5, app_command_buf);
+
+
+    spi_write(0x08, RF_FRR_CTL_A_MODE_4_data);    // disable all fast response register
+    xil_printf("disable all fast response register\r\n");
+
+    // spi_write(0x0D, RF_PREAMBLE_TX_LENGTH_9_data); // set Preamble
+ 	app_command_buf[0] = 0x11;
+	app_command_buf[1]  = 0x10;
+	app_command_buf[2]  = 0x09;
+	app_command_buf[3]  = 0x00;
+	app_command_buf[4]  = 0x08;							//  8 bytes Preamble
+	app_command_buf[5]  = 0x14;							//  detect 20 bits
+	app_command_buf[6]  = 0x00;
+	app_command_buf[7]  = 0x0f;
+	app_command_buf[8]  = 0x31;  						//  no manchest.1010.
+	app_command_buf[9]  = 0x00;
+	app_command_buf[10]  = 0x00;
+	app_command_buf[11]  = 0x00;
+	app_command_buf[12]  = 0x00;
+	spi_write(13, app_command_buf);
+	xil_printf("Sets the value of a property.GPOUP 0 SUCCESS\r\n");
+	//  RF_SYNC_CONFIG_5_data,							// set sync
+    app_command_buf[0] = 0x11;
+	app_command_buf[1]  = 0x11;
+	app_command_buf[2]  = 0x05;
+	app_command_buf[3]  = 0x00;
+	app_command_buf[4]  = 0x01;   						// no manchest , 2 bytes
+	app_command_buf[5]  = 0x2d;   						// sync byte3
+	app_command_buf[6]  = 0xd4;							// sync byte2
+	app_command_buf[7]  = 0x00;							// sync byte1
+	app_command_buf[8]  = 0x00;							// sync byte0
+    spi_write(9, app_command_buf);
+
+	//  packet crc
+    app_command_buf[0] = 0x11;
+	app_command_buf[1]  = 0x12;
+	app_command_buf[2]  = 0x01;
+	app_command_buf[3]  = 0x00;
+	app_command_buf[4]  = 0x81;							// CRC = itu-c, enable crc
+    spi_write(5, app_command_buf);
+
+	// packet   gernale configuration
+    app_command_buf[0] = 0x11;
+	app_command_buf[1]  = 0x12;
+	app_command_buf[2]  = 0x01;
+	app_command_buf[3]  = 0x06;
+	app_command_buf[4]  = 0x02;							// CRC MSB data MSB
+    spi_write(5, app_command_buf);
+
+  	// spi_write(0x07, RF_PKT_LEN_3_data);
+    app_command_buf[0] = 0x11;
+	app_command_buf[1]  = 0x12;
+	app_command_buf[2]  = 0x03;
+	app_command_buf[3]  = 0x08;
+	app_command_buf[4]  = 0x00;
+	app_command_buf[5]  = 0x00;
+	app_command_buf[6]  = 0x00;
+    spi_write(7, app_command_buf);
+
+	app_command_buf[0] = 0x11;
+	app_command_buf[1]  = 0x12;
+	app_command_buf[2]  = 0x0c;
+	app_command_buf[3]  = 0x0d;
+	app_command_buf[4]  = 0x00;
+	app_command_buf[5]  = payload_length;
+	app_command_buf[6]  = 0x04;
+	app_command_buf[7]  = 0xaa;
+	app_command_buf[8]  = 0x00;
+	app_command_buf[9]  = 0x00;
+	app_command_buf[10]  = 0x00;
+	app_command_buf[11]  = 0x00;
+	app_command_buf[12]  = 0x00;
+	app_command_buf[13]  = 0x00;
+	app_command_buf[14]  = 0x00;
+	app_command_buf[15]  = 0x00;
+	spi_write(16, app_command_buf);					// set length of Field 1 -- 4
+
+    // spi_write(0x0C, RF_PKT_FIELD_4_LENGTH_12_8_8_data);
+    app_command_buf[0] = 0x11;
+	app_command_buf[1]  = 0x12;
+	app_command_buf[2]  = 0x08;
+	app_command_buf[3]  = 0x19;
+	app_command_buf[4]  = 0x00;
+	app_command_buf[5]  = 0x00;
+	app_command_buf[6]  = 0x00;
+	app_command_buf[7]  = 0x00;
+	app_command_buf[8]  = 0x00;
+	app_command_buf[9]  = 0x00;
+	app_command_buf[10]  = 0x00;
+	app_command_buf[11]  = 0x00;
+    spi_write(12, app_command_buf);
+    xil_printf("initialize si4463 is running \r\n");
+
+    spi_write(0x10, RF_MODEM_MOD_TYPE_12_data);
+	spi_write(0x05, RF_MODEM_FREQ_DEV_0_1_data);
+
+    spi_write(0x10, RF_MODEM_TX_RAMP_DELAY_12_data);
+    spi_write(0x10, BCR_NCO_OFFSET_2_12_data);
+	spi_write(0x10, RF_MODEM_TX_RAMP_DELAY_12_data);
+    spi_write(0x07, RF_MODEM_AFC_LIMITER_1_3_data);
+	//spi_write(0x10, BCR_NCO_OFFSET_2_12_data);
+
+    spi_write(0x05, RF_MODEM_AGC_CONTROL_1_data);
+    spi_write(0x10, AGC_WINDOW_SIZE_12_data);
+    spi_write(0x0c, RF_MODEM_RAW_CONTROL_8_data);
+//	spi_write(0x10, AGC_WINDOW_SIZE_12_data);
+
+	// spi_write(0x05, RF_MODEM_RSSI_COMP_1_data);
+	app_command_buf[0] = 0x11;
+	app_command_buf[1] = 0x20;
+	app_command_buf[2] = 0x01;
+	app_command_buf[3] = 0x4e;
+	app_command_buf[4]  = 0x40;
+    spi_write(5, app_command_buf);
+
+    spi_write(0x10, COE13_7_0_12_data);
+    spi_write(0x10, COE1_7_0_12_data);
+    spi_write(0x10, COE7_7_0_12_data);
+
+	// RF_PA
+	app_command_buf[0] = 0x11;
+	app_command_buf[1]  = 0x22;
+	app_command_buf[2]  = 0x04;
+	app_command_buf[3]  = 0x00;
+	app_command_buf[4]  = 0x08;
+	app_command_buf[5]  = 127;							// set max power
+	app_command_buf[6]  =0x00;
+	app_command_buf[7]  = 0x3d;
+    spi_write(8, app_command_buf);
+
+	spi_write(0x0B, RF_SYNTH_PFDCP_CPFF_7_data);
+
+   	// header match
+   	app_command_buf[0] = 0x11;
+	app_command_buf[1]  = 0x30;
+	app_command_buf[2]  = 0x0c;
+	app_command_buf[3]  = 0x00;
+	app_command_buf[4]  = 's';
+	app_command_buf[5]  = 0xff;
+	app_command_buf[6]  = 0x40;
+	app_command_buf[7]  = 'w';
+	app_command_buf[8]  = 0xff;
+	app_command_buf[9]  = 0x01;
+	app_command_buf[10] = 'w';
+	app_command_buf[11]  =0xff;
+	app_command_buf[12]  =0x02;
+	app_command_buf[13]  = 'x';
+	app_command_buf[14]  = 0xff;
+	app_command_buf[15]  =0x03;
+    spi_write(16, app_command_buf);
+
+	spi_write(6, RF_MODEM_RAW_SEARCH2_2_data);
+    spi_write(12, RF_FREQ_CONTROL_INTE_8_data); 	    // set frequency
+    xil_printf("initialize si4463 success\r\n");
+}
+/*---------------------------------------------------------------------------*/
+ /*
+ *
+ *
+ * 使用Si4463发送数据tx_data()
+ *
+ *
+ * ---------------------------------------------------------------------------*/
+void tx_data(void)
+{
+	int i;
+	Flag.is_tx = 1;
+	fifo_reset();
+	spi_write_fifo();
+	enable_tx_interrupt();
+	clr_interrupt();
+
+	tx_start();
+	xil_printf("Si4463 start transmit data \r\n");
+	rf_timeout = 0;
+	Flag.rf_reach_timeout = 0;
+
+	i = 0;
+	/*
+	while(nIRQ)
+	{
+		ms_delay(1);
+		i++;
+		if(i>500)
+		{
+			xil_printf("Tx_data timeout , reset and initialize si4463\r\n");
+			Si4463_Reset();
+ 			Si4463_Init();
+			break;
+		}
+	}
+	*/
+	Flag.is_tx = 0;
+	xil_printf("Si4463 success Transmit data\r\n");
+}
+/*---------------------------------------------------------------------------*/
+ /*
+ *
+ *
+ * 清空RF4463FIFO，以便于存入待发送数据
+ *
+ *
+ * ---------------------------------------------------------------------------*/
+void fifo_reset(void)			// reset FIFO
+{
+	u8 p[2];
+
+	p[0] = FIFO_INFO;
+	p[1] = 0x03;   // reset tx ,rx fifo
+	spi_write(2,p);
+}
+/*---------------------------------------------------------------------------*/
+ /*
+ *
+ *
+ * 将待发送的数据存入FIFO
+ *
+ *
+ * ---------------------------------------------------------------------------*/
+void spi_write_fifo(void)
+{
+	u8 i;
+
+	i = 0;
+	while(i!=0xff)
+		i = check_cts();
+	//nSEL = 1;
+	XGpio_DiscreteSet(&Gpio,GPIO_CHANNEL,nSEL);
+	//SCK = 0;
+	XGpio_DiscreteClear(&Gpio,GPIO_CHANNEL,SCLK);
+	//nSEL = 0;
+	XGpio_DiscreteClear(&Gpio,GPIO_CHANNEL,nSEL);
+	spi_byte(WRITE_TX_FIFO);
+	for (i = 0; i< payload_length; i++)
+	{
+		spi_byte(tx_ph_data[i]);
+	}
+	//nSEL = 1;
+	XGpio_DiscreteSet(&Gpio,GPIO_CHANNEL,nSEL);
+}
+/*---------------------------------------------------------------------------*/
+ /*
+ *
+ *
+ * 配置4463 的发射中断，告知模块发射完毕数据后置低NIRQ
+ *
+ *
+ * ---------------------------------------------------------------------------*/
+void enable_tx_interrupt(void)
+{
+	u8 p[6];
+
+	p[0] = 0x11;
+	p[1] = 0x01;
+	p[2] = 0x02;
+	p[3] = 0x00;
+	p[4] = 0x01;
+	p[5] = 0x20;
+	spi_write(0x06, p);
+}
+/*---------------------------------------------------------------------------*/
+ /*
+ *
+ *
+ * 清空中断，避免前一部分操作产生的中断影响程序
+ *
+ *
+ * ---------------------------------------------------------------------------*/
+void clr_interrupt(void)		// clar interrupt
+{
+	u8 p[4];
+
+	p[0] = GET_INT_STATUS;
+	p[1] = 0;   // clr  PH pending
+	p[2] = 0;   // clr modem_pending
+	p[3] = 0;   // clr chip pending
+	spi_write(4,p);
+	spi_read(9,GET_INT_STATUS);
+}
+/*---------------------------------------------------------------------------*/
+ /*
+ *
+ *
+ * 进入发射状态，发射FIFO 中的数据，NIRQ 变高
+ *
+ *
+ * ---------------------------------------------------------------------------*/
+void tx_start(void)
+{
+	u8 p[5];
+
+	p[0] = START_TX ;
+	p[1] = freq_channel ; 		//	channel 0
+	p[2] = 0x30;
+	p[3] = 0;
+	p[4] = 0;
+	spi_write(5, p);
+}
